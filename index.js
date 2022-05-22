f4e2beade71ac027e73065049c2e70af2a6a1e8e
Tabitha Poppenwimer <poppenwimer.tkat@ufl.edu>
2022-05-22 13:17:51 +0300

    javascript greet added

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function greet() {
+    let yourName = prompt("What is your name?");
+
+    alert("Hello, " + yourName);
+
+    let yourAge = prompt("How old are you?"); 
+
+    let numberAge = parseInt(yourAge);
+
+    let response = confirm("Have you had a birthday yet this year?");
+
+    if ( response == true ) {       yearResult=
+        new Date().getFullYear() - numberAge;
+        alert("You were born in" + yearResult);
+    }
+    if ( response == false ) { yearResult=
+        new Date().getFullYear() - numberAge - 1;
+        alert("You were born in" + yearResult);
+    }
+        
+}
